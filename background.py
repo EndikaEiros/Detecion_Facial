bb1c1535120fd156bdd8590c6580d5ad9884125f
Endika Eiros <endika.eiros@gmail.com>
2023-11-09 15:25:53 +0100

    background codigo ejemplo/prueba

---
+++ background.py
@@ -0,0 +1,57 @@
+import cv2
+import time
+import numpy as np
+
+
+def bgremove(frame, min_thres, min_satur, min_brigth):
+    min_piel = np.array([min_thres, min_satur, min_brigth])
+    max_piel = np.array([255, 255, 255])
+
+    # Convert image to HSV
+    frameHSV = cv2.GaussianBlur(frame, (7, 7), 0)
+    frameHSV = cv2.cvtColor(frameHSV, cv2.COLOR_RGB2HSV)
+
+    # mascara que pilla el color del tono de piel
+    skinRegion = cv2.inRange(frameHSV, min_piel, max_piel)
+    frame_skin = cv2.bitwise_and(frame, frame, mask=skinRegion)
+
+    return frame_skin
+
+
+def main():
+    window_name = "Window"
+
+    cap = cv2.VideoCapture(0)
+    cv2.namedWindow(window_name)
+    cv2.moveWindow(window_name, 20, 20)
+
+    # Crating the trackbars
+    cv2.createTrackbar("Min", window_name, 0, 255, lambda x: x)  # 90
+    cv2.createTrackbar("Min-s", window_name, 0, 255, lambda x: x)  # 60
+    cv2.createTrackbar("Min-b", window_name, 0, 255, lambda x: x)  # 50
+
+    while True:
+        _, img = cap.read()
+
+        masked_img = bgremove(
+            img,
+            cv2.getTrackbarPos("Min", window_name),
+            cv2.getTrackbarPos("Min-b", window_name),
+            cv2.getTrackbarPos("Min-s", window_name),
+        )
+
+        cv2.imshow(window_name, masked_img)
+
+        key = cv2.waitKey(1)
+        if key == ord("q"):
+            break
+        elif key == ord("s"):
+            name = "skin" + time.asctime() + ".jpg"
+            cv2.imwrite(name, masked_img)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
