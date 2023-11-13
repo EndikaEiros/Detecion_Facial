fb622a6d18d427fec23331de9e82f024e6fa3b31
Alex Platas <48363345+alexanderplatas@users.noreply.github.com>
2023-11-13 20:12:15 +0100

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -11,17 +11,18 @@ import cv2
 import pickle 
 import numpy as np
 import cv2
+from PIL import ImageFont, ImageDraw, Image
 
 
 
-def load_model():
+def load_model(model_path):
     """
     Este método carga el modelo preentrenado
 
     :return Modelo entrenado 
 
     """
-    with open("./models/knn.model", "rb") as knn:
+    with open(model_path, "rb") as knn:
         model = pickle.load(knn)
 
     return model
@@ -43,40 +44,41 @@ def draw_square(frame, x, y, h, w, name):
     :param name
 
     """
-    cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,255), 5)
-    cv2.putText(frame, name, (x, y+h+35), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=(0,255,255))
+    cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,255), 6)
+    #cv2.putText(frame, name, (x, y+h+70), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=4, color=(0,255,0))
+    pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    ImageDraw.Draw(pil_img).text((x, y+h), name, font=ImageFont.truetype("Lato-Black.ttf", 100), fill='#ffff00')
+    frame = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
     return frame
 
 
-def bgremove(frame):
-    min_piel = np.array([90, 60, 50])
+def bgremove(frame, min_thres=90, min_satur=60, min_brigth=50):
+    
+    # Valores limite del color de la piel
+    min_piel = np.array([min_thres, min_satur, min_brigth])
     max_piel = np.array([255, 255, 255])
 
-    # Convert image to HSV
+    # Se aplica un blurr Gausioano para eliminar ruido y se convierte a HSV
     frameHSV = cv2.GaussianBlur(frame, (7, 7), 0)
     frameHSV = cv2.cvtColor(frameHSV, cv2.COLOR_RGB2HSV)
 
-    # mascara que pilla el color del tono de piel
+    # mascara que obtine el color del tono de piel del frame
     skinRegion = cv2.inRange(frameHSV, min_piel, max_piel)
     frame_skin = cv2.bitwise_and(frame, frame, mask=skinRegion)
 
     return frame_skin
 
-
-
 # Load video
-cap = cv2.VideoCapture('./data/videos/videoChorra3.mp4')
+cap = cv2.VideoCapture('../data/videos/DOS.MOV')
 
 #faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_alt.xml')
 
-success = True
+success, frame = cap.read()
 c = 0
 while success:
     
-    success, frame = cap.read()
-    
-    frame =  imutils.resize(frame, width=640)
+    #frame =  imutils.resize(frame, width=640)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -84,26 +86,30 @@ while success:
 
     faces = faceClassif.detectMultiScale(gray,1.3,5)
 
-    model = load_model()
+    model = load_model("../models/knn.model")
 
     for (x,y,w,h) in faces:
         
-        rostro = auxFrame[y:y+h,x:x+w]
 
-        rostro = cv2.resize(rostro, (32, 32), interpolation=cv2.INTER_AREA).reshape(1, -1)
+        #frame_masked = bgremove(auxFrame, 70, 30, 30)[y-10:y + h+10, x-10:x + w+10]
+        frame_masked = auxFrame[y:y + h, x:x + w]
+
+        rostro = cv2.resize(frame_masked, (32, 32), interpolation=cv2.INTER_AREA).reshape(1, -1)
         clase = model.predict(rostro)
 
         if clase[0] == 0: name = 'Alex'
         else: name = "Endika"
 
+        
         frame = draw_square(frame=frame, x=x, y=y, w=w, h=h, name=name)
+        
+    
+    cv2.imshow('frame', frame)
 
-        if len(faces) > 1:
-            cv2.imwrite(f"./prueba/doble_cara_{c}.jpg", frame)
-            c += 1
-
-    cv2.imshow('frame',frame)
+    success, frame = cap.read()
 
     k =  cv2.waitKey(1)
     if k == 27:
         break
+
+cap.release()
