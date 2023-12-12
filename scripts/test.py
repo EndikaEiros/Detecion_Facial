227f584e7189bfb0e32b6e4d33de0268fbb947ca
Alex Platas <48363345+alexanderplatas@users.noreply.github.com>
2023-12-12 17:36:14 +0100

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -3,6 +3,9 @@ import cv2
 from Recognition import get_faces
 import imutils
 import face_recognition
+from imutils import face_utils
+import dlib
+
 
 def test_landmarks():
 
@@ -64,3 +67,28 @@ def test_rotation():
 
     cap.release()
     cv2.destroyAllWindows()
+
+def get_drawed_landmarks_from_image()
+         
+    p = "models/shape_predictor_68_face_landmarks.dat"
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(p)
+    
+    image = cv2.imread('data/keanu.png')
+     
+    # while True:
+        # _, image = cap.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+    rects = detector(gray, 0)
+    
+    for (i, rect) in enumerate(rects):
+    
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+    
+        for (x, y) in shape:
+            cv2.circle(image, (x, y), 1, (0, 255, 0), -1)
+    
+    cv2.imshow("Salida", image)
+    cv2.imwrite('data/keanu_landmarks.png',image)
